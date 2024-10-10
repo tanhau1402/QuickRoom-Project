9bf5259adc9a12119243bab9f34e0aa15f59e3ce
buitanhau <hau48912@gmail.com>
2024-10-10 08:04:47 +0700

    update 10/10/2024

--- src/pages/clients/Detail.jsx
+++ src/pages/clients/Detail.jsx
@@ -1,29 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-
+import { db } from "../../services/firebase";
+import { doc, getDoc } from "firebase/firestore";
 
 function Detail(props) {
     const { id } = useParams();
+    const [room, setRoom] = useState({ imgUrls: [] });
+    const [loading, setLoading] = useState(true);
+    useEffect(() => {
+        const fetchRoomData = async () => {
+            try {
+                const roomDoc = doc(db, "listRooms", id);
+                const roomSnap = await getDoc(roomDoc);
 
-    console.log(id);
+                if (roomSnap.exists()) {
+                    setRoom(roomSnap.data()); // Lưu dữ liệu vào state
+                } else {
+                    console.log("No such document!");
+                }
+            } catch (error) {
+                console.error("Error fetching room data: ", error);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchRoomData();
+    }, [id]);
     return (
         <div>
-            <div className="card-title mx-auto flex justify-around p-4">
-                <div className="left flex  ">
-                    <i class="fa-solid fa-house text-xl"></i>
-                    <p className="font-light text-2xl">
+            <div className="card-title container mx-auto grid grid-cols-2  md:grid-cols-2 lg:grid-cols-2 p-4 mt-4">
+                <div className="left flex justify-start  ">
+                    <i class="fa-solid fa-house text-xl me-2"></i>
+                    <p className="font-sans text-2xl">
                         Ở trong ngôi nhà Purple Rain của Prince
                     </p>
                 </div>
-                <div className="group-action flex">
-                    <div className="act-1 me-3">
-                        <i class="fa-regular fa-share-from-square"></i>
+                <div className="group-action flex justify-end me-3">
+                    <div className="act-1 me-5 text-xl   hover:text-blue-800 cursor-pointer">
+                        <i class="fa-regular fa-share-from-square hover:scale-150 duration-300  me-1"></i>
                         Share
                     </div>
-
-                    <div className="act-2">
-                        <i class="fa-regular fa-heart"></i>
-                        Like
+                    <div className="act-2 text-xl hover:text-red-800 cursor-pointer">
+                        <i class="fa-regular fa-heart me-1 hover:scale-150 duration-300  "></i>
+                        <span>Like</span>
                     </div>
                 </div>
             </div>
@@ -31,25 +51,24 @@ function Detail(props) {
                 {/* Hình ảnh lớn bên trái */}
                 <div className="box-left">
                     <img
-                        className="w-full   rounded-2xl"
+                        className="w-full hover:scale-105 duration-300   rounded-2xl"
                         style={{ height: "415px" }}
-                        src="https://a0.muscache.com/im/pictures/hosting/Hosting-U3RheVN1cHBseUxpc3Rpbmc6MTEzMTA4OTQ5ODA0MDcwMTE4Mw%3D%3D/original/a766e0e9-1e6f-4b88-b8d5-ce12375c6de8.png?im_w=2560&im_q=highq"
+                        src={room.imgUrls[0]}
                         alt=""
                     />
                 </div>
-
                 {/* Hình ảnh nhỏ bên phải */}
                 <div className="box-right grid grid-cols-2 gap-2">
                     {/* Hình trên */}
                     <div className="top-left flex flex-col gap-2">
                         <img
-                            className="w-full h-auto rounded-tl-2xl "
-                            src="https://a0.muscache.com/im/pictures/hosting/Hosting-U3RheVN1cHBseUxpc3Rpbmc6MTEzMTA4OTQ5ODA0MDcwMTE4Mw%3D%3D/original/a766e0e9-1e6f-4b88-b8d5-ce12375c6de8.png?im_w=2560&im_q=highq"
+                            className="w-full hover:scale-105 duration-300 h-auto rounded-tl-2xl "
+                            src={room.imgUrls[1]}
                             alt=""
                         />
                         <img
-                            className="w-full h-auto rounded-bl-2xl "
-                            src="https://a0.muscache.com/im/pictures/hosting/Hosting-U3RheVN1cHBseUxpc3Rpbmc6MTEzMTA4OTQ5ODA0MDcwMTE4Mw%3D%3D/original/a766e0e9-1e6f-4b88-b8d5-ce12375c6de8.png?im_w=2560&im_q=highq"
+                            className="w-full hover:scale-105 duration-300 h-auto rounded-bl-2xl "
+                            src={room.imgUrls[2]}
                             alt=""
                         />
                     </div>
@@ -57,29 +76,45 @@ function Detail(props) {
                     {/* Hình dưới */}
                     <div className="bottom-left flex flex-col gap-2">
                         <img
-                            className="w-full h-auto rounded-tr-2xl"
-                            src="https://a0.muscache.com/im/pictures/hosting/Hosting-U3RheVN1cHBseUxpc3Rpbmc6MTEzMTA4OTQ5ODA0MDcwMTE4Mw%3D%3D/original/a766e0e9-1e6f-4b88-b8d5-ce12375c6de8.png?im_w=2560&im_q=highq"
+                            className="w-full hover:scale-105 duration-300 h-auto rounded-tr-2xl"
+                            src={room.imgUrls[3]}
                             alt=""
                         />
                         <img
-                            className="w-full h-auto rounded-br-2xl"
-                            src="https://a0.muscache.com/im/pictures/hosting/Hosting-U3RheVN1cHBseUxpc3Rpbmc6MTEzMTA4OTQ5ODA0MDcwMTE4Mw%3D%3D/original/a766e0e9-1e6f-4b88-b8d5-ce12375c6de8.png?im_w=2560&im_q=highq"
+                            className="w-full hover:scale-105 duration-300 h-auto rounded-br-2xl"
+                            src={room.imgUrls[4]}
                             alt=""
                         />
                     </div>
                 </div>
             </div>
-            <div class="container grid-container mx-auto p-5 gap-5  ">
+            <div class="container r grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 mx-auto p-5 gap-5  ">
                 <div className="main-left">
                     <div class="header">
                         <p className="text-2xl font-bold me-3">
                             Minneapolis, Minnesota, Hoa Kỳ
                         </p>
-                        <p className="text-sm text-gray-600">
-                            4 khách · 2 phòng ngủ · 2 giường · 2 phòng tắm
+                        <p className="text-2xl text-green-700 font-light me-3">
+                            {room.available}
                         </p>
-                    </div>
 
+                        <hr className="mt-2 mb-2" />
+                        <p className="text-xl font-semibold">Thông tin phòng :</p>
+                        <p className="text-lg font-mono text-gray-600">
+                            <i class="fa-solid fa-person text-2xl me-2"></i>
+                            {room.persons} khách
+                        </p>
+                        <p className="text-lg font-mono text-gray-600">
+                            <i class="fa-solid fa-dollar-sign text-2xl me-2"></i>
+                            {room.price_per_night} USD/đêm
+                        </p>
+                        <p className="text-lg font-mono text-gray-600">
+                            <i class="fa-solid fa-paw text-2xl me-2"></i>
+                            {room.animals ? "Cho nuôi thú cưng" : "Không cho nuôi thú cưng"}
+                        </p>
+                        <hr className="mt-2 mb-2" />
+                    </div>
+                    
                     <div class="host flex items-center space-x-2 mt-3">
                         <img
                             src="https://a0.muscache.com/im/pictures/user/User-571409646/original/ea5debfb-2394-444c-ae7a-e30e13363e59.jpeg?im_w=240"
@@ -115,18 +150,120 @@ function Detail(props) {
 
                         • Ngon giấc như Prince trong phòng ngủ của The Kid, được trang hoàng y hệt như trong bộ phim. Hoặc thức để khám phá bộ sưu tập băng nhạc cá nhân của ông, bao gồm một trong những bản thu âm thử chính gốc của Prince. <br />
                     </div>
-                    .
+                    <div className="user-info">
+                        <hr className="mt-7" />
+                        <p className="text-2xl mt-8 mb-5">Gặp gỡ Chủ nhà</p>
+                        <div className="wrap hover:scale-110 duration-300 text-center w-96 bg-white-600 h-auto p-4 rounded-2xl shadow-2xl">
+                            <div className="relative">
+                                <img
+                                    src="https://a0.muscache.com/im/pictures/user/User-571409646/original/ea5debfb-2394-444c-ae7a-e30e13363e59.jpeg?im_w=240"
+                                    alt="Avatar"
+                                    class="w-24 h-24 rounded-full mx-auto"
+                                />
+                                <i style={{ right: "117px" }} class="fa-solid absolute bottom-2  text-3xl text-pink-600  fa-circle-check"></i>
+                            </div>
+                            <p className="font-semibold text-xl">Wendy And Lisa</p>
+                            <p className="font-extralight">Bắt đầu đón tiếp khách từ năm 2024</p>
+                        </div>
+                        <div className="user-list mt-4">
+                            <div className="job mb-3 ">
+                                <i class="fa-solid fa-briefcase me-3 text-2xl text-gray-600"></i>
+                                <span className="font-extralight text-lg">Công việc của tôi: Nghệ sĩ âm nhạc/Nhạc sĩ</span>
+                            </div>
+                            <div className="language mb-3 text-2xl ">
+                                <i class="fa-solid fa-language me-3 text-gray-600"></i>
+                                <span className="font-extralight text-lg">Nói Tiếng Anh</span>
+                            </div>
+                            <div className="living mb-3 text-2xl">
+                                <i class="fa-solid fa-earth-americas me-3 text-gray-600"></i>
+                                <span className="font-extralight text-lg"> Sống tại Los Angeles, California</span>
+                            </div>
+                            <div className="user-des font-thin">
+                                Xin chào! Chúng tôi là Wendy và Lisa. Chúng tôi là bộ đôi âm nhạc từng đoạt giải thưởng, những người bạn từ thuở nhỏ, và thành viên đáng tự hào của ban nhạc rock huyền thoại The Revolution. Hồi những năm 1980, chúng tôi đã hợp tác với người bạn thân, cũng là trưởng nhóm Revolution, Prince, để đưa ca khúc và bộ phim "Purple Rain" trứ danh trở thành hiện thực. Chúng tôi vô cùng háo hức khi được ngược dòng thời gian về nơi chứng kiến tất cả, và chia sẻ với bạn những kỷ niệm còn kỳ diệu hơn nữa!
+                            </div>
+                        </div>
+                        <hr className="mt-7" />
+                    </div>
+                    <div className="bed-room mt-7">
+                        <span className="text-2xl mt-8 mb-5">Nơi bạn sẽ ngủ nghỉ</span>
+                        <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-2 gap-2 mt-5">
+                            <div className="room-1 ">
+                                <img
+                                    className="w-full h-auto mb-3 rounded-2xl"
+                                    src="https://a0.muscache.com/im/pictures/hosting/Hosting-U3RheVN1cHBseUxpc3Rpbmc6MTEzMTA4OTQ5ODA0MDcwMTE4Mw%3D%3D/original/a766e0e9-1e6f-4b88-b8d5-ce12375c6de8.png?im_w=2560&im_q=highq"
+                                    alt=""
+                                />
+                                <p className="font-medium text-lg ">Phòng ngủ 1</p>
+                                <span className="font-extralight">1 giường queen</span>
+                            </div>
+                            <div className="room-2 ">
+                                <img
+                                    className="w-full h-auto mb-3 rounded-2xl"
+                                    src="https://a0.muscache.com/im/pictures/hosting/Hosting-U3RheVN1cHBseUxpc3Rpbmc6MTEzMTA4OTQ5ODA0MDcwMTE4Mw%3D%3D/original/a766e0e9-1e6f-4b88-b8d5-ce12375c6de8.png?im_w=2560&im_q=highq"
+                                    alt=""
+                                />
+                                <p className=" font-medium text-lg ">Phòng ngủ 2</p>
+                                <span className="font-extralight">1 giường đôi</span>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div className="main-right ">
                     <div class="bg-gray-200 p-4 rounded-lg">
                         <p class="text-center text-gray-700 font-bold">
-                            Đã dừng nhận đặt phòng
+                            Nhận đặt phòng
                         </p>
-                        <button class="mt-4 w-full bg-gray-400 text-white py-2 rounded-lg">
+                        <button class="mt-4 w-full hover:scale-105 duration-300 hover:bg-green-500 bg-gray-400 text-white py-2 rounded-lg">
                             Yêu cầu
                         </button>
                     </div>
                 </div>
+            </div>
+            <div className="highlight container  mx-auto p-5 gap-5 ">
+                <hr className="mb-4" />
+                <p className="font-semibold mb-3">Điểm nổi bật trong khu vực</p>
+                <p className="font-extralight">Bạn muốn biết mình sẽ đến đâu?? Bạn sẽ có mặt tại CHÍNH căn nhà Purple Rain nơi quê nhà của The Kid, ở Minneapolis, Minnesota! CHÍNH là nơi đó. Chúng tôi đã trang hoàng cho nơi này như một địa điểm tưởng nhớ di sản bất hủ của người bạn của chúng tôi, cũng như bộ phim kinh điển được nhiều người yêu mến. Hãy sẵn sàng để cảm nhận phép màu của The Kid trong sắc tím hoàng gia thời đỉnh cao.</p>
+                <hr className="mt-6" />
+            </div>
+            <div className="booking container mx-auto grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-2 p-5">
+                <div className="box-1">
+                    <i class="fa-regular fa-envelope text-3xl"></i>
+                    <p className="font-semibold mt-1 mb-1">Yêu cầu đặt phòng</p>
+                    <p className="font-extralight">Chọn ngày mong muốn, thêm khách, sau đó trả lời câu hỏi về lý do bạn muốn đi.</p>
+                </div>
+                <div className="box-2">
+                    <i class="fa-regular fa-circle-check text-3xl"></i>
+                    <p className="font-semibold mt-1 mb-1">Quy trình lựa chọn</p>
+                    <p className="font-extralight">Trước tiên, chúng tôi sẽ chọn ngẫu nhiên một nhóm khách tiềm năng. Tiếp theo, chúng tôi sẽ xem xét câu trả lời của nhóm khách này để rút ra những quan điểm và cảm nhận riêng của họ về nơi ở biểu tượng. Sau đó, chúng tôi sẽ mời những khách được chọn đặt phòng..</p>
+                </div>
+                <div className="box-3">
+                    <i class="fa-regular fa-address-card text-3xl"></i>
+                    <p className="font-semibold mt-1 mb-1">Yêu cầu cần đáp ứng</p>
+                    <p className="font-extralight">Để tham gia, bạn phải có tài khoản Airbnb đang hoạt động và đã tải ứng dụng; đồng thời, bạn phải là cư dân của một quốc gia hoặc khu vực hợp lệ. Việc gửi yêu cầu là hoàn toàn miễn phí.</p>
+                </div>
+            </div>
+            <div className="terms container mx-auto p-5">
+                <hr className="mb-4" />
+                <p className="text-sm font-light text-gray-500">Nếu bạn được chọn và quyết định đặt phòng, bạn sẽ có 24 giờ để hoàn tất giao dịch. Không bao gồm chi phí đi lại. Vui lòng tham khảo <br /> quy tắc đầy đủ, bao gồm quy định về độ tuổi và vị trí địa lý đủ điều kiện, cách thức dữ liệu sẽ được sử dụng, xác suất được chọn và các <br /> điều khoản khác.</p>
+                <hr className="mt-4" />
+            </div>
+            <div className="clauses container mx-auto   p-5">
+                <p className="text-2xl font-semibold">Những điều cần biết</p>
+                <div className="clauses-item grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-2">
+                    <div className="clauses-1 mt-5">
+                        <p className="font-bold mb-2">Nội quy nhà</p>
+                        <p className="mb-2">Nhận phòng sau 16:00</p>
+                        <p className="mb-2">Trả phòng trước 11:00</p>
+                        <p>Tối đa 4 khách</p>
+                    </div>
+                    <div className="clauses-2 mt-5">
+                        <p className="font-bold mb-2">An toàn và chỗ ở</p>
+                        <p className="mb-2">Chỗ ở có camera an ninh ngoài nhà</p>
+                        <p className="mb-2">Máy phát hiện khí CO</p>
+                        <p>Máy báo khói</p>
+                    </div>
+                </div>
+                <hr className="mt-5 mb-3" />
             </div>
         </div>
     );
